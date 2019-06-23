5070b840eeeb5d5541b653a271f6fb489073674a
Mitsuru Nakada <nakada@geckolink.com>
2019-06-23 14:55:58 +0900

    config fileをjson->jsに変更

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  setupWebServerPort: 4080,
+  controllerHost: 'localhost',
+  controllerPort: 47900,
+  registryHost: 'https://geckolink.com',
+  localIPRegistPort: 443,
+  applicationPort: 5000,
+  basePath: '/var/gecko',
+  logFile: '/var/gecko/log/home_server.log',
+  defaultPassword: '3b8d0b97514d2519c71664785a04e050d8090e7a616ec3c1374982d173e950ab',
+  powerLED: false,
+  hap: true,
+  motor: false,
+  led: false,
+  smartMeter: true,
+  wisunDevice: '/dev/ttyUSB0',
+};
